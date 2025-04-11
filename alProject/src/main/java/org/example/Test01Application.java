179a3d10095229a5b2f047101c7963d9b6382092
langyang <lang.yang@geegobyte.com>
2025-04-11 14:37:42 +0800

    [add]SecurityContext基础代码

---
+++ alProject/src/main/java/org/example/Test01Application.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test01Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Test01Application.class, args);
+    }
+}
